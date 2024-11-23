7c54877829763125407869e818a7f9677ccf331d
Sahil pillania <sahilpillania88@gmail.com>
2024-11-23 14:24:15 +0400

    added login/logout dynamic

--- src/main/java/com/enotes/controller/UserController.java
+++ src/main/java/com/enotes/controller/UserController.java
@@ -2,6 +2,7 @@ package com.enotes.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,7 @@ import com.enotes.repository.UserRepository;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+	@Autowired
 	private UserRepository userRepo;
 	
 	@ModelAttribute
